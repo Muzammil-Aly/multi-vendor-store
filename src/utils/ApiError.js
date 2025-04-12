74125a1262141c75aa12fb98d32b66fe1af277c1
Muzammil <muzammilnizaami9@gmail.com>
2025-04-12 12:49:13 +0500

    feat: add cookie-parser and cors middleware, implement ApiError and ApiResponse classes, and create asyncHandler utility

---
+++ src/utils/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went Wrong!!",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.errors = errors;
+    this.data = null;
+    this.message = false; // we are sending errors not response message
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
