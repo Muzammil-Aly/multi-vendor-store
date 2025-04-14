fcc60ef650adfc30b569b7f0483fde621f8eb794
Muzammil <muzammilnizaami9@gmail.com>
2025-04-14 10:01:23 +0500

    feat: create product model with schema definition for e-commerce functionality

---
+++ src/models/product.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const ProductSchema = new Schema({
+  id: String,
+  url: String,
+  detailUrl: String,
+  title: {
+    shortTitle: String,
+    longTitle: String,
+  },
+  price: {
+    mrp: Number,
+    cost: Number,
+    discount: String,
+  },
+  description: String,
+  discount: String,
+  tagline: String,
+});
+
+export const Product = mongoose.model("Poduct", ProductSchema);
